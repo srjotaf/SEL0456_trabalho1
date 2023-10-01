8e9cff9e3f5e6abb6fca93fb63dba074de142c4c
srjotaf <srjotafrancisco@gmail.com>
2023-10-01 20:39:16 -0300

    funcoes de adicionar e remover

--- arrays.c
+++ arrays.c
@@ -10,8 +10,34 @@ double* criar_vetor(int tamanho_vetor){
     return vetor;
     
 }
-int adicionar_elemento(){}
-int remover_elemento(){}
+int adicionar_elemento(double * vetor, int* tamanho_vetor){
+    double novo_elemento;
+    printf("\nQual é o elemento que você deseja adicionar? ");
+    scanf("%lf", &novo_elemento);
+    int posicao;
+    printf("Em qual posicao vocẽ deseja adicionar o elemento %lf? ", novo_elemento);
+    scanf("%d", &posicao);
+    if (posicao>=(*tamanho_vetor)){
+        vetor=realloc(vetor, posicao+1);
+        printf("O vetor tinha %d elementos e agora terá %d elementos.\n", (*tamanho_vetor), posicao+1);
+        printf("O elemento %lf será inserido na posicao %d.\n", novo_elemento, posicao);
+        vetor[posicao]=novo_elemento;
+        (*tamanho_vetor)=posicao+1;
+    }
+    else if (posicao<(*tamanho_vetor)){
+        printf("O elemento %lf será inserido na posicao %d.\n", novo_elemento, posicao);
+        vetor[posicao]=novo_elemento;
+    }
+    
+}
+int remover_elemento(double * vetor){
+    int posicao;
+    printf("\nQual é a posicao cujo elemento vocẽ deseja remover? ");
+    scanf("%d", &posicao);
+    printf("O elemento da posição %d será removido (substituído por zero).\n", posicao);
+    vetor[posicao]=0;
+    
+}
 
 int main(){
     int tamanho_vetor;
@@ -23,6 +49,7 @@ int main(){
     int sair=0;
     char operacao;
     do{
+        system("clear");
         printf("\nO vetor é: \n");
         printf("Posição    Valor\n");
         for (int i=0;i<tamanho_vetor;i++){
@@ -30,14 +57,14 @@ int main(){
         }
         printf("\nPara adicionar um novo elemento digite 'A'.\n");
         printf("Para remover um elemento digite 'R'.\n");
-        printf("Para sair digite qualquer outra letra.");
+        printf("Para sair digite qualquer outra letra. ");
         scanf(" %c", &operacao);
         switch (operacao){
         case 'A':
-            adicionar_elemento();
+            adicionar_elemento(vetor, &tamanho_vetor);
             break;
         case 'R':
-            remover_elemento();
+            remover_elemento(vetor);
             break;
         default:
             sair=1;
